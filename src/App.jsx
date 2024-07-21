9889af6684906b9f10f80a0050b94eaf5bb8d027
shanME <giorgie103188@gmail.com>
2024-07-21 15:39:13 +0400

    dinamic content

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,29 @@ import Header from "./components/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 
 import { CORE_CONCEPTS } from "./data.js";
+import { EXAMPLES } from "./data.js";
 import TabButtons from "./components/TabButtons.jsx";
 import { useState } from "react";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("Please click a button");
+  const [selectedTopic, setSelectedTopic] = useState();
 
   const handleClick = (selecteButton) => {
     setSelectedTopic(selecteButton);
   };
+
+  let content = "Please click a button";
+  if (selectedTopic) {
+    content = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -32,14 +46,14 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButtons onSelect={() => handleClick("component")}>
+            <TabButtons onSelect={() => handleClick("components")}>
               Component
             </TabButtons>
             <TabButtons onSelect={() => handleClick("jsx")}>JSX</TabButtons>
             <TabButtons onSelect={() => handleClick("props")}>Props</TabButtons>
             <TabButtons onSelect={() => handleClick("state")}>State</TabButtons>
           </menu>
-          {selectedTopic}
+          {content}
         </section>
       </main>
     </div>
